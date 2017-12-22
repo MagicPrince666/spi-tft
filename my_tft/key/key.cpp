72c7ac5bcf84c9c11a70c4349b67272e6b10ffc3
prince <prince@princedeMacBook-Pro.local>
2017-12-22 17:44:12 +0800

    delete raindow

--- my_tft/key/key.cpp
+++
@@ -1,8 +0,0 @@
-#include "key.h"
-#include "sys.h"
-
-void Init_Key()
-{
-    mt76x8_gpio_set_pin_direction(39, 0);
-    mt76x8_gpio_set_pin_value(39, 1);
-} 
--- my_tft/key/key.h
+++
@@ -1,8 +0,0 @@
-#ifndef _KEY_H_
-#define _KEY_H_
-
-#define key mt76x8_gpio_get_pin(39)
-
-void Init_Key();
-
-#endif
