3c024505700de5a0df6f3a69966ffbd193a8cea9
prince <prince@princedeMacBook-Pro.local>
2018-06-08 13:57:23 +0800

    first char bug

--- spi/spi.cpp
+++ spi/spi.cpp
@@ -18,7 +18,7 @@
 static const char *device = "/dev/spidev32766.1";
 static uint8_t mode = 0; /* SPI通信使用全双工，设置CPOL＝0，CPHA＝0。 */
 static uint8_t bits = 8; /* 8ｂiｔｓ读写，MSB first。*/
-static uint32_t speed = 96 * 1000 * 1000;/* 设置96M传输速度 */
+static uint32_t speed = 40 * 1000 * 1000;/* 设置96M传输速度 */
 int g_SPI_Fd = 0;
 
 
